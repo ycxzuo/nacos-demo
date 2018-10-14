f0df5c8c47c591c495a09b8227723891689fbd1f
ycxzuo <343720929@qq.com>
2018-10-14 23:46:21 +0800

    提交基础框架

---
+++ nacos-client/src/main/java/com/yczuoxin/nocosclient/NacosClientApplication.java
@@ -0,0 +1,7 @@
+package com.yczuoxin.nocosclient;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosClientApplication {
+}
---
+++ nacos-server/src/main/java/com/yczuoxin/nacosserver/NacosServerApplication.java
@@ -0,0 +1,12 @@
+package com.yczuoxin.nacosserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosServerApplication.class, args);
+    }
+}
